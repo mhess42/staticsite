502f7f5417bd064737b2e677e602d9524be2fff5
mhess42 <madelinehess42@gmail.com>
2024-04-18 17:58:53 -0400

    implemented generate_pages_recursive

--- src/main.py
+++ src/main.py
@@ -28,14 +28,28 @@ def generate_page(from_path, template_path, dest_path):
     html = markdown_to_html_node(md).to_html()
     title = extract_title(md)
     page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
-    # os.makedirs('/'.join(dest_path.split('/')[:-1]))
+    dirs = '/'.join(dest_path.split('/')[:-1])
+    if not os.path.exists(dirs):
+        os.makedirs(dirs)
     with open(dest_path, 'w') as dest:
         dest.write(page)
         dest.close()
 
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for node in os.listdir(dir_path_content):
+        path = os.path.join(dir_path_content, node)
+        if os.path.isfile(path):
+            generate_page(path, template_path, os.path.join(dest_dir_path, node).replace('.md', '.html'))
+        else:
+            generate_pages_recursive(path, template_path, os.path.join(dest_dir_path, node))
+
+
 def main():
     move_static(static)
-    generate_page('content/index.md', 'template.html', 'public/index.html')
+    # generate_page('content/index.md', 'template.html', 'public/index.html')
+    generate_pages_recursive('content/', 'template.html', 'public/')
     return 
+
 
 main()
