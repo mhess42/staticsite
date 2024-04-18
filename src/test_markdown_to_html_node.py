d8c3c31dc26203980300972d914e8f2d06b80a4b
mhess42 <madelinehess42@gmail.com>
2024-04-18 16:45:24 -0400

    added and passes mix test for markdown_to_html_node

--- src/test_markdown_to_html_node.py
+++ src/test_markdown_to_html_node.py
@@ -166,3 +166,27 @@ class TestHTMLNode(unittest.TestCase):
         e = '<div><ol><li>this is an ol</li><li>that spans across</li><li>multiple lines</li></ol></div>'
         c = markdown_to_html_node(md).to_html()
         self.assertEqual(e, c)
+    
+    def test_mix1(self):
+        md = '''
+        # the following will be a mix of md
+        
+        1. this is the first element in my list
+        2. this is the second element in my list
+        
+        ### however in no particular order
+        
+        - this should work
+        - and so should this
+        
+        though as i once said
+        
+        >unit testing freaking sucks
+        
+        ```
+        because you have to write so much code
+        ```
+        '''
+        e = '<div><h1>the following will be a mix of md</h1><ol><li>this is the first element in my list</li><li>this is the second element in my list</li></ol><h3>however in no particular order</h3><ul><li>this should work</li><li>and so should this</li></ul><p>though as i once said</p><blockquote>unit testing freaking sucks</blockquote><pre><code>because you have to write so much code</code></pre></div>'
+        c = markdown_to_html_node(md).to_html()
+        self.assertEqual(e, c)
